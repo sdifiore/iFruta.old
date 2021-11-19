52391d53f584c8a9cba069165bd508a3628c512d
Sergio Di Fiore <difiores@outlook.com>
2021-11-19 15:36:41 -0300

    Add project files.

---
+++ Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace iFruta.Models
+{
+    
+    [Table("Categorias")]
+    public class Categoria
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(16)]
+        public string Nome { get; set; } = string.Empty;
+
+        [StringLength(128)]
+        public string Descricao { get; set;} = string.Empty;
+    }
+}
